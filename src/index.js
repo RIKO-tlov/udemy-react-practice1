ab3cc7250eb2a501df430eec38914525fbc8eaed
riko <79884989+RIKO-tlov@users.noreply.github.com>
2021-09-22 22:47:52 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,179 @@
+/**
+ * const, let等の変数宣言
+ */
+
+// var val1 = "val変数";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数の再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// //const変数は上書き不可
+// val3 = "const変数を上書き";
+
+// const val3 = "const変数再宣言";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "riko",
+//   age: 24,
+// };
+// val4.name = "りこ";
+// val4.address = "aichi"
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "riko";
+// const age = 24;
+
+// const message1 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message1);
+
+/**
+ * アロー関数
+ */
+// const func2 = (str) => str;
+// console.log(func2("funcです。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//   name: "riko",
+//   age: 24,
+// };
+
+// const message1 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message2);
+
+// const myProfile =['riko', 24];
+// const message3 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const[name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("riko");
+
+/**
+ * スプレット構文...
+ */
+
+//配列の展開
+// const arr1 = [1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2,...arr3 ] = arr2
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+// const nameArr = ["riko", "yume", "arisa"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`))
+
+// nameArr.map((name) => console.log(name));
+
+// const numArr =[1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "riko") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// const val1 = 1 > 0 ? 'trueです。' : 'falseです。';
+// console.log(val1);
+
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : "数値を入力してください。" ;
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!" : "許容範囲内です。";
+};
+console.log(checkSum(50, 40));
